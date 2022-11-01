0f170bb4ea5b1a2d8f631e1ea788b045fe7ed304
Luke Holroyd <lukejholroyd@gmail.com>
2022-11-01 13:52:39 +1300

    Fix fetch URL

--- components/hooks/useSearch.ts
+++ components/hooks/useSearch.ts
@@ -5,7 +5,7 @@ export function useSearch() {
   const [queryCache, setQueryCache] = useState<{[query: string]: Replay[]}>({});
 
   const searchIndex = async (query: string, index: string) => {
-    const url = `https://ssg-refactor.pages.dev/api/search/${index}?q=${query}`;
+    const url = `/api/search/${index}?q=${query}`;
 
     if (queryCache[url]) {
       return queryCache[url];
