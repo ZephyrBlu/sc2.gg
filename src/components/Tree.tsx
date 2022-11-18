53cd475797b094575c3aaa4d6d7d660e42f16bd8
Luke Holroyd <lukejholroyd@gmail.com>
2022-11-19 10:41:01 +1300

    Add arrows for tree rendering

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -11,6 +11,7 @@ export function Tree({ race, oppRace, tree }){
 
     const node_buildings = node.label.split(',');
     stack.push(...node_buildings);
+
     // rendered.push({offset, build: stack});
     // console.log('pushing fragment', node_buildings);
 
@@ -30,14 +31,37 @@ export function Tree({ race, oppRace, tree }){
   return (
     <div className="Tree">
       {rendered.map(({ offset, build }) => (
-        <div className="Tree__branch" style={{marginLeft: 30 * offset}}>
+        <div className="Tree__branch" style={{marginLeft: (50 * offset) - 20}}>
+          {/* {offset !== 0 &&
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="Tree__arrow"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25" />
+            </svg>} */}
           {build.map((building) => (
-            <img
-              alt={building}
-              title={building}
-              className="Tree__building-icon"
-              src={`/images/buildings/${race}/${building}.png`}
-            />
+            <div className="Tree__building">
+              <img
+                alt={building}
+                title={building}
+                className="Tree__building-icon"
+                src={`/images/buildings/${race}/${building}.png`}
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="Tree__arrow"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+              </svg>
+            </div>
           ))}
         </div>
       ))}
