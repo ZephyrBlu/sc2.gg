489ed12b5eee8c2406a347242c0707fa0bd618df
Luke Holroyd <lukejholroyd@gmail.com>
2022-11-23 01:17:42 +1300

    Start working on BFS rendering for trees

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,8 +1,13 @@
+import {useRef} from 'react';
 import './Tree.css';
 
-export function Tree({ race, tree }){  
-  const rendered = [];
-  const renderNodes = (node, stack = [], offset = 0) => {
+export function Tree({ race, tree }) {
+  const renderedDfs = [];
+  const renderNodesDfs = (
+    node,
+    stack = [],
+    offset = 0,
+  ) => {
     if (node.label === '') {
       return;
     }
@@ -11,20 +16,83 @@ export function Tree({ race, tree }){
     stack.push(...node_buildings);
 
     if (node.children.length === 0) {
-      rendered.push({offset, build: stack});
+      if (stack.length + offset >= 5) {
+        renderedDfs.push({offset, build: stack});
+      }
       return;
     }
-    renderNodes(node.children[0], [...stack], offset);
-    node.children.slice(1).forEach(child => renderNodes(child, [], stack.length + offset));
-    
-    // node.children.forEach(child => renderNodes(child, [], stack.length + offset));
-  }
 
-  tree.root.children.forEach(child => renderNodes(child));
+    // node.children.forEach(child => {
+    //   renderNodes(child, [], stack.length + offset, prefix);
+    // });
+
+    // renderNodes(node.children[0], [...stack], offset, prefix);
+    node.children.slice(0).forEach(child => renderNodesDfs(child, [...stack], offset));
+  };
+
+  let renderedBfs = {};
+  const MAX_BRANCHES = 5;
+  const MIN_TOTAL = 25;
+  const renderNodesBfs = (node) => {
+    let queue = [{node, prefix: ''}];
+    let branches = 0;
+    while (queue.length > 0 && branches < MAX_BRANCHES) {
+      const {node, prefix} = queue[0];
+
+      let newPrefix = `${prefix},${node.label}`;
+
+      if (branches < MAX_BRANCHES) {
+        node.children.forEach(child => {
+          if (child.total > MIN_TOTAL) {
+            queue.push({node: child, prefix: `${newPrefix},${child.label}`});
+            branches += 1;
+          }
+        });
+      }
+
+      queue = queue.slice(1);
+    }
+
+    const dfs = (node, prefix, stack = [], offset = 0) => {
+      if (node.label === '') {
+        return;
+      }
+
+      const node_buildings = node.label.split(',');
+      stack.push(...node_buildings);
+
+      if (node.children.length === 0) {
+        renderedBfs[prefix].push({offset, build: stack});
+        return;
+      }
+
+       node.children.forEach(child => {
+        dfs(child, prefix, [], stack.length + offset);
+      });
+    };
+
+    console.log('queue', queue);
+    queue.forEach(({node, prefix}) => {
+      console.log('node, prefix', node, prefix);
+      renderedBfs[prefix.slice(1)] = [];
+      dfs(node, prefix.slice(1));
+    });
+
+    console.log('raw bfs', renderedBfs);
+    renderedBfs = Object.entries(renderedBfs).map(([prefix, nodes]) => {
+      const initialNode = {offset: 0, build: prefix.split(',')};
+      return [initialNode, ...nodes];
+    });
+  };
+
+  // tree.root.children.forEach(child => renderNodesDfs(child));
+  tree.root.children.forEach(child => renderNodesBfs(child));
+
+  console.log('bfs rendered', renderedBfs[0]);
 
   return (
     <div className="Tree">
-      {rendered.map(({ offset, build }) => (
+      {renderedBfs[0].map(({ offset, build }) => (
         <div className="Tree__branch" style={{marginLeft: 50 * offset}}>
           {/* {offset !== 0 &&
             <svg
