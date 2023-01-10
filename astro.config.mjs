3e0fcd21e94edf9105e9ed22fa87362634cda07c
Luke Holroyd <lukejholroyd@gmail.com>
2023-01-10 17:57:18 +1300

    Remove astrojs image component from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+// import react from '@astrojs/react';
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
-import image from "@astrojs/image";
+// import image from "@astrojs/image";
 
 export default defineConfig({
   integrations: [
     // react(),
     preact({compat: true}),
-    image(),
+    // image(),
   ]
 });
