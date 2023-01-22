cd5495d6dfa487b438bf93ad1cc35c0d9bf62b9d
Luke Holroyd <lukejholroyd@gmail.com>
2023-01-22 15:02:35 +1300

    Basic styling on build search link + better spacing in report

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -200,48 +200,51 @@ export function Tree({ race, opponentRace, tree }) {
     matchup.sort();
 
     return (
-      <div className="Tree">
-        <div className="Tree__header">
-          <div className="Tree__modifiers Tree__modifiers--secondary">
-            <div className="Tree__modifier Tree__modifier--secondary">
-              {Math.round((rootNode.node.total.wins / rootNode.node.total.total) * 1000) / 10}% winrate
-            </div>
-            <div className="Tree__modifier Tree__modifier--secondary">
-              {Math.round((rootNode.node.total.total / tree.root.total.total) * 1000) / 10}% playrate
-            </div>
-            <div className="Tree__modifier Tree__modifier--secondary">
-              {rootNode.node.total.total} games
+      <>
+        <div className="Tree">
+          <div className="Tree__header">
+            <div className="Tree__modifiers Tree__modifiers--secondary">
+              <div className="Tree__modifier Tree__modifier--secondary">
+                {Math.round((rootNode.node.total.wins / rootNode.node.total.total) * 1000) / 10}% winrate
+              </div>
+              <div className="Tree__modifier Tree__modifier--secondary">
+                {Math.round((rootNode.node.total.total / tree.root.total.total) * 1000) / 10}% playrate
+              </div>
+              <div className="Tree__modifier Tree__modifier--secondary">
+                {rootNode.node.total.total} games
+              </div>
             </div>
           </div>
+          <div className="Tree__prefix">
+            {prefix.map((building, index) => (
+              <div className="Tree__building">
+                <img
+                  alt={building}
+                  title={building}
+                  className="Tree__building-icon"
+                  src={`/images/buildings/${race}/${building}.png`}
+                />
+                {prefix.length - 1 !== index &&
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="Tree__arrow"
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                  </svg>}
+              </div>
+            ))}
+          </div>
+          {/* {renderChildren(rootNode.node, prefix.length)} */}
+          <a className="Tree__search-opening" href={`/?matchup=${matchup.join('')}&build=${prefix.join(',')}`}>
+            Search for {race.slice(0, 1)}v{opponentRace.slice(0, 1)} games with this opening
+          </a>
         </div>
-        <div className="Tree__prefix">
-          {prefix.map((building, index) => (
-            <div className="Tree__building">
-              <img
-                alt={building}
-                title={building}
-                className="Tree__building-icon"
-                src={`/images/buildings/${race}/${building}.png`}
-              />
-              {prefix.length - 1 !== index &&
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="Tree__arrow"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                </svg>}
-            </div>
-          ))}
-        </div>
-        {/* {renderChildren(rootNode.node, prefix.length)} */}
-        <a className="Tree__search-opening" href={`/?matchup=${matchup.join('')}&build=${prefix.join(',')}`}>
-          See {race.slice(0, 1)}v{opponentRace.slice(0, 1)} games with this opening
-        </a>
-      </div>
+        <hr className="Builds__cluster-divider Builds__cluster-divider--tree" />
+      </>
     );
   });
 
